84545cc523fc4cb12a71615d794fcc15ddd6019a
Anas Rchid <archid-@stdent.1337.ma>
2023-03-23 21:21:00 +0000

    deleted users and channels when destroyed

--- headers/command.hpp
+++ headers/command.hpp
@@ -6,7 +6,7 @@
 /*   By: mhanda <mhanda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 17:10:03 by archid            #+#    #+#             */
-/*   Updated: 2023/03/22 04:43:47 by mhanda           ###   ########.fr       */
+//   Updated: 2023/03/27 21:21:45 by archid           ###   ########.fr       //
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,46 +15,42 @@
 #include <vector>
 #include <string>
 
-// class command {
-// public:
-// 	typedef command *pointer;
-
-// 	command(std::string cmd);
-
-// 	void set_args(std::vector<std::string> args) { args_ = args; }
-
-// 	virtual void exec(std::vector<std::string> params) = 0;
-
-// 	virtual  bool is_command(std::string cmd) = 	0;
-
-// protected:
-// 	std::vector<std::string> args_;
-// };
-
-// class user_command : public command {
-// public:
-// 	user_command() : command("user") {}
-
-// 	virtual void exec(std::vector<std::string> params) {}
-
-// 	virtual bool is_command(std::string cmd) {}
-// };
-
-// extern std::vector<command::pointer> cmds;
-
 class command {
+public:
 
-	public:
-		typedef command *pointer;
+	command(std::vector<std::string> parts) : params_(parts) {}
 
-		command(std::vector<std::string> parts) : parts_(parts) {}
+	virtual int exec() const = 0;
 
-		virtual int exec() const = 0;
+	const std::vector<std::string> &params() const { return params_; }
 
-		std::vector<std::string> parts_;
-
-		const std::vector<std::string> &parts() const { return parts_; }
+protected:
+	std::vector<std::string> params_;
 };
 
-command::pointer parse_command(const std::string &msgs);
-int exec_command(const std::string &msgs);
+class user : public command {
+};
+
+class nick : public command {
+};
+
+class pass : public command {
+};
+
+class kick : public command {
+};
+
+class join : public command {
+};
+
+class privmsg : public command {
+};
+
+class oper : public command {
+};
+
+class mode : public command{
+};
+
+command *parse_command(const std::string &msg, int fd);
+int exec_command(const std::string &msg);
--- srcs/command.cpp
+++ srcs/command.cpp
@@ -6,7 +6,7 @@
 /*   By: mhanda <mhanda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/25 01:11:23 by archid            #+#    #+#             */
-//   Updated: 2023/03/22 19:15:39 by archid           ###   ########.fr       //
+//   Updated: 2023/03/27 21:22:01 by archid           ###   ########.fr       //
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,40 +14,42 @@
 #include "server.hpp"
 #include "user.hpp"
 
-void authenticate(const std::string &msg, const int fd)
-{
-
-    user *tmp = NULL;
-
-    tmp = map_users.at(fd);
-    if (msg.find("NICK ") != std::string::npos && tmp->nickname().empty())
+void authenticate(const std::string &msg, const int fd) {
+	user *usr = ircserv.user(fd);
+	if (msg.find("NICK ") != std::string::npos && usr->nickname().empty())
     {
-        if (msg.length() > 6){
-            tmp->nickname(msg.substr(5));
-            std::cout << "nick name set\n";
-            return ;
-        }
+			if (msg.length() > 6){
+				usr->nickname(msg.substr(5));
+				std::cout << "nick name set\n";
+				return ;
+			}
     }
 
-    if (msg.find("USER ") != std::string::npos && tmp->username().empty()) {
-        if (msg.length() > 6)
-        {
-            tmp->username(msg.substr(5));
-            std::cout << "user set\n";
-            return ;
-        }
-    }
+	if (msg.find("USER ") != std::string::npos && usr->username().empty()) {
+		if (msg.length() > 6)
+			{
+				usr->username(msg.substr(5));
+				std::cout << "user set\n";
+				return ;
+			}
+	}
 
-    if (msg.find("PASS ") != std::string::npos)
+	if (msg.find("PASS ") != std::string::npos)
     {
-        if (msg.length() > 6){
-            std::string tmp1 = msg.substr(5);
-            if (tmp1 == "123")
-            {
-                return ;
-            }
-            std::cerr << "464 " << "PASS" << " :Password incorrect" << tmp1 <<"|"<< std::endl;
-    }
-    send(fd, "wrong arg\n", 10, 0);
+			if (msg.length() > 6){
+				std::string tmp1 = msg.substr(5);
+				if (tmp1 == "123")
+					{
+						return ;
+					}
+				std::cerr << "464 " << "PASS" << " :Password incorrect" << tmp1 <<"|"<< std::endl;
+			}
+			send(fd, "wrong arg\n", 10, 0);
+		}
 }
+
+command *parse_command(const std::string &msg, int fd) {
+}
+
+int exec_command(const std::string &msg) {
 }
